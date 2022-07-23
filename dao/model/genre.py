40b697265568891cc4e49567e0cbfacc2c14217e
MrLerich <93153582+MrLerich@users.noreply.github.com>
2022-07-23 15:21:17 +0300

    fix models

--- dao/model/genre.py
+++ dao/model/genre.py
@@ -7,9 +7,15 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text())
 
+    def __str__(self):
+        """ Отображает название жанра во вьюшках /movies"""
+        return self.name
+
 
 class GenreSchema(Schema):
+    id = fields.Int()
     name = fields.Str()
 
 
 genre_schema = GenreSchema()
+genres_schema = GenreSchema(many=True)
