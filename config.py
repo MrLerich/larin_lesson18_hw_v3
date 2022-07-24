6928a88058fcaffe72281f10a0866e5593fbcb4d
MrLerich <93153582+MrLerich@users.noreply.github.com>
2022-07-25 00:07:23 +0300

    fix models,schema, views, dao v3

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_AS_ASCII = False
-    RESTX_JSON = {'ensure_ascii': False, 'indent': 2}
+    RESTX_JSON = {'ensure_ascii': False, 'indent': 4}
 
 
